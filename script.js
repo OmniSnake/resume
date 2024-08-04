2417526e9fc8b1d38ed92c18dcac3abe4964b710
Omni <help.ominc@gmail.com>
2024-08-05 03:46:32 +0500

    feat: add Material Wave effect on click

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
     document.querySelectorAll('[contenteditable]').forEach((element, index) => {
         const savedContent = sessionStorage.getItem('content' + index);
         if (savedContent) {
@@ -6,6 +7,22 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         element.addEventListener('input', () => {
             sessionStorage.setItem('content' + index, element.innerHTML);
+        });
+
+        element.addEventListener('click', function (element) {
+            const ripple = document.createElement('span');
+            ripple.classList.add('ripple');
+            this.appendChild(ripple);
+
+            const maxSize = Math.max(this.clientWidth, this.clientHeight);
+            ripple.style.width = ripple.style.height = `${maxSize}px`;
+            const rect = this.getBoundingClientRect();
+            ripple.style.left = `${element.clientX - rect.left - maxSize / 2}px`;
+            ripple.style.top = `${element.clientY - rect.top - maxSize / 2}px`;
+
+            ripple.addEventListener('animationend', () => {
+                ripple.remove();
+            });
         });
     });
 });
